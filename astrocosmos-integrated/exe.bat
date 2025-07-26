2aa122ed8bd0cee364290b8cb301044009dc12b4
William Skyfurs <199439308+williamskyfurs@users.noreply.github.com>
2025-07-26 21:49:26 +0700

    Updated and Tested Exe Files

---
+++ astrocosmos-integrated/exe.bat
@@ -0,0 +1,42 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCmDJHiU5E45KxpARQuMAEi1ArwS79Tc5v6vt08hZucvd5zn+6eaMu8Q5VbpeoUR92lOisQPAwhVcQGCYBsIqH1WpGuGONSRvBysQ0ad40wiGnFxg1/Rnzg+dMBUjMoQxiGy73HTkKIfkVvwUqYcEGevyK9nWA==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQImeFUGAlTPPm6uB/tNiA==
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQIROw9GQweNL2a1FfU15vz+6PiGtkgZNA==
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCmDJHiU5E45KxpARQuMAEi1ArwS79Tc5v6vt08hZucvd5zn+6eaMu8Q5VbpeoUR92lOisQPAwhVcQGCYBsIqH1WpGuGONSRvBysQ0ad40wiGnFxg1/Rnz8pb9Bnic4MwG238ln6mLcRxX37EKwWEAM=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+
+:: ---
+
+:: Astrocosmos Integrated Launch Sequence
+:: This launcher opens the looping presentation in windowed mode
+
+@echo off
+cd /d "%~dp0"
+start "" "assets\astrocosmos-integrated.ppsx"
