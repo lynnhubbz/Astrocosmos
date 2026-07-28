02c67396d8db3995673f121e606ac56f98018acf
Lynn Hubbz <williamskyfurs@gmail.com>
2026-07-28 19:15:41 +0700

    QUARTZ-TEST

---
+++ content/__init__.py
@@ -0,0 +1,3 @@
+# File for ACCESS POINT??
+# Containing scripts to translate catalogue data to the application
+# basically an API/Entry point
---
+++ content/submission/README.md
@@ -0,0 +1,3 @@
+folder for public to submit their creations.
+
+make a new folder with your name, and follow the template
